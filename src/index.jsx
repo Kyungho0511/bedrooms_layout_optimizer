0888770a7e88fcf2bc73c1ca6e428e0b1418d0e5
Kyungho0511 <kleearchitecture@gmail.com>
2023-11-24 12:45:35 -0500

    web Navbar added

--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,19 @@
-import "./style.css";
 import ReactDOM from "react-dom/client";
 import { Canvas } from "@react-three/fiber";
 import Experience from "./Experience.jsx";
-import * as THREE from "three";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
-// const created = ({ gl, scene }) => {
-//   console.log(scene, gl);
-//   scene.background = new THREE.Color("red");
-//   // gl.setClearColor("#ff0000", 1);
-// };
 
 root.render(
   <Canvas
     shadows
     camera={{
-      fov: 45,
+      zoom: 70,
       near: 0.1,
-      far: 200,
-      position: [-4, 3, 6],
+      far: 100,
+      position: [5, 5, 5],
     }}
-    // onCreated={created}
+    orthographic
   >
     <Experience />
   </Canvas>
